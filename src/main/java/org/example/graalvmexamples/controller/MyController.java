7eb789a8228405d2e2dbdac8b8298957199654e4
BO <bwang0629@gmail.com>
2024-10-12 22:04:38 +0800

    update

--- src/main/java/org/example/graalvmexamples/controller/MyController.java
+++ src/main/java/org/example/graalvmexamples/controller/MyController.java
@@ -1,4 +1,19 @@
 package org.example.graalvmexamples.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/")
 public class MyController {
+
+    @GetMapping("index")
+    public Map<String, Object> index() {
+        return Map.of("message", "Hello World");
+    }
 }
